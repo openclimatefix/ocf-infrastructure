eeb0889900562ff812755c69296afb4495362f49
peterdudfield <peter.dudfield@hotmail.com>
2022-01-11 16:48:24 +0000

    done #4

--- terraform/README.md
+++ terraform/README.md
@@ -1,5 +1,13 @@
+To setup the project:
 ```bash
 terraform init
+```
+To push changes:
+```bash
 terraform plan
 terram apply
 ```
+
+You can then destroy the stack:
+```bash
+terraform destroy```
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     encrypt = true
     bucket = "nowcasting-terraform"
-    dynamodb_table = "terraform-state-lock-dynamo"
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
