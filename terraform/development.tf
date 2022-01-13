346e6380f12a0874a0dad22edfdbf89d69527d07
peterdudfield <peter.dudfield@hotmail.com>
2022-01-13 11:58:01 +0000

    fmt terraform

--- terraform/development.tf
+++ terraform/development.tf
@@ -8,26 +8,26 @@ locals {
 module "networking" {
   source = "./modules/networking"
 
-  region               = "${var.region}"
-  environment          = "${var.environment}"
-  vpc_cidr             = "${var.vpc_cidr}"
-  public_subnets_cidr  = "${var.public_subnets_cidr}"
-  private_subnets_cidr = "${var.private_subnets_cidr}"
-  availability_zones   = "${local.production_availability_zones}"
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+  availability_zones   = local.production_availability_zones
 }
 
 module "s3" {
   source = "./modules/s3"
 
-  region               = "${var.region}"
-  environment          = "${var.environment}"
+  region      = var.region
+  environment = var.environment
 }
 
 module "ecs" {
   source = "./modules/ecs"
 
-  region               = "${var.region}"
-  environment          = "${var.environment}"
+  region      = var.region
+  environment = var.environment
 }
 
 #module "api" {
@@ -51,8 +51,8 @@ module "ecs" {
 module "nwp" {
   source = "./modules/services/nwp"
 
-  region                    = "${var.region}"
-  environment               = "${var.environment}"
-  iam-policy-s3-nwp-write   = module.s3.iam-policy-s3-nwp-write
-  s3-bucket = module.s3.s3-nwp-bucket
+  region                  = var.region
+  environment             = var.environment
+  iam-policy-s3-nwp-write = module.s3.iam-policy-s3-nwp-write
+  s3-bucket               = module.s3.s3-nwp-bucket
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -4,5 +4,5 @@ environment = "development"
 
 /* module networking */
 vpc_cidr             = "10.0.0.0/16"
-public_subnets_cidr  = ["10.0.1.0/24"] //List of Public subnet cidr range
-private_subnets_cidr = ["10.0.10.0/24","10.0.11.0/24"] //List of private subnet cidr range
+public_subnets_cidr  = ["10.0.1.0/24"]                  //List of Public subnet cidr range
+private_subnets_cidr = ["10.0.10.0/24", "10.0.11.0/24"] //List of private subnet cidr range
