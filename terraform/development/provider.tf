76bc9a639b21d51eeef4a500d293553773955038
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-02-23 12:47:09 +0000

    🐛  Workaround for env vars in CF provider

--- terraform/development/provider.tf
+++ terraform/development/provider.tf
@@ -7,7 +7,7 @@ terraform {
 
     cloudflare = {
       source = "cloudflare/cloudflare"
-      version = "~> 3.0"
+      version = "~> 3.9.1"
     }
   }
 
@@ -18,7 +18,16 @@ provider "aws" {
   region = var.region
 }
 
+
+variable "cloudflare_email" {
+  description = "Email Address of the Cloudflare account"
+}
+
+variable "cloudflare_api_token" {
+  description = "API Token of the Cloudflare account"
+}
+
 provider "cloudflare" {
-  # email is populated via env var
-  # api_token is populated via env var
+  email = var.cloudflare_email
+  api_token = var.cloudflare_api_token
 }
