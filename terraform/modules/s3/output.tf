15dbb02352ee66575837474043a1ed53e99a3f3c
Jacob Bieker <jacob@bieker.tech>
2022-03-08 10:39:57 +0000

    Update output.tf

--- terraform/modules/s3/output.tf
+++ terraform/modules/s3/output.tf
@@ -10,6 +10,18 @@ output "s3-nwp-bucket" {
   value = aws_s3_bucket.s3-nwp-bucket
 }
 
+output "iam-policy-s3-sat-write" {
+  value = aws_iam_policy.iam-policy-s3-sat-write
+}
+
+output "iam-policy-s3-sat-read" {
+  value = aws_iam_policy.iam-policy-s3-sat-read
+}
+
+output "s3-sat-bucket" {
+  value = aws_s3_bucket.s3-sat-bucket
+}
+
 output "iam-policy-s3-ml-read" {
   value = aws_iam_policy.iam-policy-s3-ml-read
 }
