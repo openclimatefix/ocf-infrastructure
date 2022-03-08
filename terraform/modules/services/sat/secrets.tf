3217ad6631423ffc86d27072e272d0f42dc7c1cb
Jacob Bieker <jacob@bieker.tech>
2022-03-08 10:19:32 +0000

    Update secrets.tf

--- terraform/modules/services/sat/secrets.tf
+++ terraform/modules/services/sat/secrets.tf
@@ -2,10 +2,8 @@
 
 data "aws_secretsmanager_secret" "sat-api" {
   name = "development/consumer/sat"
-  arn = "arn:aws:secretsmanager:eu-west-2::secret:development/consumer/sat"
 }
 
 data "aws_secretsmanager_secret_version" "sat-api-version" {
   secret_id = data.aws_secretsmanager_secret.sat-api.id
-  arn = "arn:aws:secretsmanager:eu-west-2::secret:development/consumer/sat"
 }
