29e861eb2d79ecb4aef70e44d1548ee24a44c955
peterdudfield <peter.dudfield@hotmail.com>
2022-05-16 11:28:24 +0100

    allow forecasdt to read satellite s3

--- terraform/modules/services/forecast/variables.tf
+++ terraform/modules/services/forecast/variables.tf
@@ -13,6 +13,10 @@ variable "iam-policy-s3-nwp-read" {
   description = "IAM policy to read to s3 bucket for NWP data"
 }
 
+variable "iam-policy-s3-sat-read" {
+  description = "IAM policy to read to s3 bucket for Satellite data"
+}
+
 variable "iam-policy-s3-ml-read" {
   description = "IAM policy to read to s3 bucket for ML models"
 }
