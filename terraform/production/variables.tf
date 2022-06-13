263982fd497899f67a16f63a6e35dba7958e3123
peterdudfield <peter.dudfield@hotmail.com>
2022-06-13 21:38:01 +0100

    add production

---
+++ terraform/production/variables.tf
@@ -0,0 +1,56 @@
+variable "region" {
+  description = "The AWS region to use"
+}
+
+variable "environment" {
+  description = "The Production environment"
+}
+
+//Networking
+variable "vpc_cidr" {
+  description = "The CIDR block of the vpc"
+}
+
+variable "public_subnets_cidr" {
+  type        = list(string)
+  description = "The CIDR block for the public subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(string)
+  description = "The CIDR block for the private subnet"
+}
+
+variable "api_version" {
+  description = "The API version"
+}
+
+variable "data_visualization_version" {
+  description = "The Data Visualization version"
+}
+
+variable "forecast_version" {
+  description = "The Forecast version"
+}
+
+variable "nwp_version" {
+  description = "The NWP version"
+}
+
+variable "sat_version" {
+  description = "The Satellite version"
+}
+
+variable "pv_version" {
+  description = "The PV Consumer version"
+}
+
+variable "gsp_version" {
+  description = "The GSP Consumer version"
+  default = "0.0.2"
+}
+
+
+variable "cloudflare_zone_id" {
+  description = "The ZoneID of the nowcasting domain"
+}
