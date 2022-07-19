dc17279da07ef789925ea492c1cee72aa09482fd
peterdudfield <peter.dudfield@hotmail.com>
2022-07-19 22:29:15 +0100

    udpaet gsps to 317

--- terraform/modules/services/gsp/ecs.tf
+++ terraform/modules/services/gsp/ecs.tf
@@ -24,7 +24,7 @@ resource "aws_ecs_task_definition" "gsp-task-definition" {
       environment : [
         { "name" : "LOGLEVEL", "value" : "DEBUG"},
         { "name" :"REGIME", "value" : "in-day"},
-        { "name" :"N_GSPS", "value" : "339"}
+        { "name" :"N_GSPS", "value" : "317"}
       ]
 
       secrets : [
