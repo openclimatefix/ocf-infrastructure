f4b3791f361be122d608e45960f818ddcc259274
peterdudfield <peter.dudfield@hotmail.com>
2022-07-28 17:07:36 +0100

    use template_file

--- terraform/modules/networking/ec2_bastion/ec2.tf
+++ terraform/modules/networking/ec2_bastion/ec2.tf
@@ -1,10 +1,14 @@
 # https://www.middlewareinventory.com/blog/terraform-aws-ec2-user_data-example/
 
+data "template_file" "user_data" {
+  template = "${file("user_data.sh")}"
+}
+
 resource "aws_instance" "ec2-bastion" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ec2-ssh.id]
-  user_data = "${file("user_data.sh")}"
+  user_data = "${data.template_file.user_data.rendered}"
   subnet_id = var.public_subnets_id
 }
 
