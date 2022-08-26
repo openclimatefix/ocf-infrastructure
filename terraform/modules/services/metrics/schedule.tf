cd2b4bfeb9a58c1372959ac1abe650fa2abf3cb1
peterdudfield <peter.dudfield@hotmail.com>
2022-08-26 10:30:06 +0200

    add metric application

---
+++ terraform/modules/services/metrics/schedule.tf
@@ -0,0 +1,33 @@
+# This is an temporary module.
+# This will schedule the aws task to run on cron job.
+# We want to move to Dagster but for the moment its useful to have this setup
+
+resource "aws_cloudwatch_event_rule" "event_rule" {
+  name                = "metrics-schedule-${var.environment}"
+  schedule_expression = "cron(* 13 * * ? *)"
+  # runs day at 13
+}
+
+resource "aws_cloudwatch_event_target" "ecs_scheduled_task" {
+
+  rule      = aws_cloudwatch_event_rule.event_rule.name
+  target_id = "metrics-schedule-${var.environment}"
+  arn       = var.ecs-cluster.arn
+  role_arn  = aws_iam_role.cloudwatch_role.arn
+
+  ecs_target {
+
+    launch_type         = "FARGATE"
+    platform_version    = "1.4.0"
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.metrics-task-definition.arn
+    network_configuration {
+
+      subnets          = var.public_subnet_ids
+      assign_public_ip = true
+
+    }
+
+  }
+
+}
