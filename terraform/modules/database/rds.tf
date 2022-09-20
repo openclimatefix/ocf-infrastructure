0658ca7267b4f9fde14a24e029f08c6d251cbf1c
peterdudfield <peter.dudfield@hotmail.com>
2022-09-20 15:27:38 +0100

    rds db t3 small

--- terraform/modules/database/rds.tf
+++ terraform/modules/database/rds.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "db-forecast" {
   max_allocated_storage        = 100
   engine                       = "postgres"
   engine_version               = "14.4"
-  instance_class               = "db.t3.micro"
+  instance_class               = "db.t3.small"
   name                         = "forecast${var.environment}"
   identifier                   = "forecast-${var.environment}"
   username                     = "main"
