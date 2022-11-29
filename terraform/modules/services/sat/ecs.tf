f949de19f18356ab2e173827c2d01f3cd9c08123
peterdudfield <peter.dudfield@hotmail.com>
2022-11-29 11:24:47 +0000

    add sat data clean up

--- terraform/modules/services/sat/ecs.tf
+++ terraform/modules/services/sat/ecs.tf
@@ -55,3 +55,63 @@ resource "aws_ecs_task_definition" "sat-task-definition" {
 
   # add volume? So we dont have to keep downloading same docker image
 }
+
+
+resource "aws_ecs_task_definition" "sat-clean-up-task-definition" {
+  family                   = "sat-clean-up"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  # specific values are needed -
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html
+  cpu    = 1024
+  memory = 5120
+
+
+  task_role_arn      = aws_iam_role.consumer-sat-iam-role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+      name  = "sat-clean-up-consumer"
+      image = "openclimatefix/satip:${var.docker_version}"
+      #      cpu       = 128
+      #      memory    = 128
+      essential = true
+
+      environment : [
+        { "name" : "SAVE_DIR", "value" : "s3://${var.s3-bucket.id}/data" },
+        { "name" : "LOG_LEVEL", "value" : "DEBUG"},
+      ]
+
+      secrets : [
+        {
+          "name" : "API_KEY",
+          "valueFrom" : "${data.aws_secretsmanager_secret_version.sat-api-version.arn}:API_KEY::",
+        },
+        {
+          "name" : "API_SECRET",
+          "valueFrom" : "${data.aws_secretsmanager_secret_version.sat-api-version.arn}:API_SECRET::",
+        },
+        {
+          "name" : "DB_URL",
+          "valueFrom" : "${var.database_secret.arn}:url::",
+        },
+        {
+          "name" : "CLEANUP",
+          "valueFrom" : "1",
+        }
+      ]
+
+      logConfiguration : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-group" : var.log-group-name,
+          "awslogs-region" : var.region,
+          "awslogs-stream-prefix" : "streaming"
+        }
+      }
+    }
+  ])
+
+  # add volume? So we dont have to keep downloading same docker image
+}
--- terraform/modules/services/sat/schedule.tf
+++ terraform/modules/services/sat/schedule.tf
@@ -31,3 +31,35 @@ resource "aws_cloudwatch_event_target" "ecs_scheduled_task" {
   }
 
 }
+
+
+# run data tailor clean up
+resource "aws_cloudwatch_event_rule" "event_rule_sat_clean_up" {
+  name                = "sat-clean-up-schedule-${var.environment}"
+  schedule_expression = "cron(0 6,18 * * ? *)"
+  # runs every 5 minutes
+}
+
+resource "aws_cloudwatch_event_target" "ecs_scheduled_task_sat_clean_up" {
+
+  rule      = aws_cloudwatch_event_rule.event_rule_sat_clean_up.name
+  target_id = "sat-clean-up-schedule-${var.environment}"
+  arn       = var.ecs-cluster.arn
+  role_arn  = aws_iam_role.cloudwatch_role.arn
+
+  ecs_target {
+
+    launch_type         = "FARGATE"
+    platform_version    = "1.4.0"
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.sat-clean-up-task-definition.arn
+    network_configuration {
+
+      subnets          = var.public_subnet_ids
+      assign_public_ip = true
+
+    }
+
+  }
+
+}
