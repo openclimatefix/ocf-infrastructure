911cd62f4b248380df7a885c71c1b2b5f1213b03
devsjc <sol@openclimatefix.org>
2023-05-03 16:48:22 +0100

    5th pass

--- terraform/nowcasting/development/main.tf
+++ terraform/nowcasting/development/main.tf
@@ -241,7 +241,7 @@ module "internal_ui" {
         subnets = module.networking.public_subnets
     }
     database_config = {
-        url = module.database.forecast-database-secret.secret.value
+        url = module.database.forecast-database-secret
         read_policy_arn = module.database.iam-policy-forecast-db-read.arn
     }
 }
