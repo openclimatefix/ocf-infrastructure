ee816e6bafb12e1ea9862e197710c42c92f43bd3
peterdudfield <peter.dudfield@hotmail.com>
2023-07-10 21:17:20 +0100

    add variables

--- terraform/airflow/development/main.tf
+++ terraform/airflow/development/main.tf
@@ -3,13 +3,13 @@ Variables used across all modules
 ======*/
 locals {
   production_availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  domain = "nowcasting"
+  domain = "airflow"
 }
 
 
 module "airflow" {
   source = "../../modules/services/airflow"
-  
+
   environment   = var.environment
   vpc_id        = var.vpc_id
   subnets       = var.subnets
---
+++ terraform/airflow/development/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  description = "The AWS region to use"
+  type = string
+}
+
+variable "environment" {
+  description = "The Deployment environment"
+  type = string
+
+  validation {
+    condition     = contains(["development", "production"], var.environment)
+    error_message = "Valid values for var: environment are (development, production)."
+  }
+}
+
+//Networking
+variable "subnets" {
+  type        = list(string)
+  description = "The CIDR block for the public subnet"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "The ID of the VPC to build the subnets upon"
+}
