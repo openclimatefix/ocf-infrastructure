a63d8be68251b81ae664ac5ada7d3b5e17cc44f8
peterdudfield <peter.dudfield@hotmail.com>
2023-07-11 13:10:38 +0100

    change to t3.medium

--- terraform/modules/services/airflow/dags/dayafter-dag.py
+++ terraform/modules/services/airflow/dags/dayafter-dag.py
@@ -8,7 +8,7 @@ from airflow.operators.latest_only import LatestOnlyOperator
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime.now(),
+    "start_date": datetime.utcnow()  - timedelta(hours=1),
     "retries": 1,
     "retry_delay": timedelta(minutes=1),
     "max_active_runs": 10,
