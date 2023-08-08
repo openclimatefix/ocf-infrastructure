121861a91ddc5092aa5938a9f25137bf127f8f55
peterdudfield <peter.dudfield@hotmail.com>
2023-08-08 18:27:11 +0100

    airflow production up to c9111d3

--- terraform/airflow/production/main.tf
+++ terraform/airflow/production/main.tf
@@ -24,7 +24,7 @@ module "airflow_subnetworking" {
 }
 
 module "airflow" {
-  source = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=7c2ae8b"
+  source = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=c9111d3"
 
   environment   = var.environment
   vpc_id        = var.vpc_id
