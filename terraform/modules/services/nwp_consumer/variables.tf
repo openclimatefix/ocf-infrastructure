46c9bab98f6b20056478ad85c6d977e6b54ddfd6
devsjc <sol@openclimatefix.org>
2023-11-09 17:27:33 +0000

    Update validation rules

--- terraform/modules/services/nwp_consumer/variables.tf
+++ terraform/modules/services/nwp_consumer/variables.tf
@@ -110,41 +110,23 @@ variable ecs-task_size {
     condition = length(keys(var.ecs-task_size)) == 2
     error_message = "Variable ecs-task_size must have exactly two keys: cpu and memory."
   }
-  // See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#data
-  // for valid CPU and memory values.
   validation {
     condition = contains(["256", "512", "1024", "2048", "4096", "8192"], var.ecs-task_size.cpu)
     error_message = "CPU must be one of 256, 512, 1024, 2048, 4096, or 8192."
   }
   validation {
-    // If the CPU is 256, the memory must be 0.5, 1, or 2 * 1024 MiB. 
-    condition = var.ecs-task_size.cpu == "256" && contains(["512", "1024", "2048"], var.ecs-task_size.memory)
-    error_message = "For CPU 256, memory must be one of 512, 1024, or 2048."
-  }
-  validation {
-    // If the CPU is 512, the memory must be between 1 and 4 GiB, in 1 GiB increments.
-    condition = var.ecs-task_size.cpu == "512" && contains(range(1024, 4096, 1024), var.ecs-task_size.memory)
-    error_message = "Fo CPU 512, memory must be one of 1024, 2048, 3072, or 4096."
-  }
-  validation {
-    // If the CPU is 1024, the memory must be between 2 and 8 GiB, in 1 GiB increments.
-    condition = var.ecs-task_size.cpu == "1024" && contains(range(2048, 8192, 1024), var.ecs-task_size.memory)
-    error_message = "For CPU 1024, memory must be one of 2048 and 8192 in 1024 increments."
-  }
-  validation {
-    // If the CPU is 2048, the memory must be between 4 and 16 GiB, in 1 GiB increments.
-    condition = var.ecs-task_size.cpu == "2048" && contains(range(4096, 16384, 1024), var.ecs-task_size.memory)
-    error_message = "Memory must be between 4096 and 16384 in 1024 increments."
-  }
-  validation {
-    // If the CPU is 4096, the memory must be between 8 and 30 GiB, in 1 GiB increments.
-    condition = var.ecs-task_size.cpu == "4096" && contains(range(8192, 30720, 1024), var.ecs-task_size.memory)
-    error_message = "Memory must be between 8192 and 30720 in 1024 increments."
-  }
-  validation {
-    // If the CPU is 8192, the memory must be between 16 and 60 GiB, in 4 GiB increments.
-    condition = var.ecs-task_size.cpu == "8192" && contains(range(16384, 61440, 4096), var.ecs-task_size.memory)
-    error_message = "Memory must be between 16384 and 61440 in 4096 increments."
+    // See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#data
+    // for valid CPU and memory values.
+    condition = (
+      var.ecs-task_size.cpu == "256" ? contains(["512", "1024", "2048"], var.ecs-task_size.memory) :
+      var.ecs-task_size.cpu == "512" ? contains(range(1024, 4096, 1024), var.ecs-task_size.memory) :
+      var.ecs-task_size.cpu == "1024" ? contains(range(2048, 8192, 1024), var.ecs-task_size.memory) :
+      var.ecs-task_size.cpu == "2048" ? contains(range(4096, 16384, 1024), var.ecs-task_size.memory) :
+      var.ecs-task_size.cpu == "4096" ? contains(range(8192, 30720, 1024), var.ecs-task_size.memory) :
+      var.ecs-task_size.cpu == "8192" ? contains(range(16384, 61440, 4096), var.ecs-task_size.memory) :
+      true
+    )
+    error_message = "Invalid combination of CPU and memory."
   }
 }
 
