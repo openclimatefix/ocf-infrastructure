beed4f5474b8a049500d2b09a1b0517ae072833d
devsjc <sol@openclimatefix.org>
2023-11-15 15:04:20 +0000

    Update policies for reading private bucket data

--- terraform/modules/storage/s3-private/iam.tf
+++ terraform/modules/storage/s3-private/iam.tf
@@ -1,26 +1,38 @@
 # Make IAM policy to read and write to the s3-private bucket for NWP
 
+data "aws_iam_policy_document" "read_policy_description" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetObjectAttributes"
+    ]
+    resources = [aws_s3_bucket.bucket.arn, "${aws_s3_bucket.bucket.arn}/*"]
+    effect = "Allow"
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
 resource "aws_iam_policy" "read-policy" {
   name        = "s3-${var.service_name}-read-policy"
   description = "Policy to read bucket: ${aws_s3_bucket.bucket.bucket}"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = ["s3:ListBucket",
-          "s3:GetObject",
-        ]
-        Effect = "Allow"
-        Resource = [
-          aws_s3_bucket.bucket.arn,
-        "${aws_s3_bucket.bucket.arn}/*"]
-      },
+  policy = data.aws_iam_policy_document.read_policy_description.json
+}
+
+data "aws_iam_policy_document" "write_policy_description" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
     ]
-  })
+    resources = [aws_s3_bucket.bucket.arn, "${aws_s3_bucket.bucket.arn}/*"]
+    effect = "Allow"
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
@@ -30,20 +42,5 @@ resource "aws_iam_policy" "write-policy" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = ["s3:ListBucket",
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
-        Effect = "Allow"
-        Resource = [
-          aws_s3_bucket.bucket.arn,
-        "${aws_s3_bucket.bucket.arn}/*"]
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.write_policy_description.json
 }
