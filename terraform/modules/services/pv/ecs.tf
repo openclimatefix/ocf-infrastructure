483d8e9df57136763854817984738541a1885252
peterdudfield <peter.dudfield@hotmail.com>
2024-01-04 10:10:57 +0000

    only pull pv data from sheffield solar

--- terraform/modules/services/pv/ecs.tf
+++ terraform/modules/services/pv/ecs.tf
@@ -20,47 +20,6 @@ resource "aws_ecs_task_definition" "pv-task-definition" {
   execution_role_arn = var.ecs-task_execution_role_arn
   container_definitions = jsonencode([
     {
-      name  = "pv-consumer"
-      image = "openclimatefix/pvconsumer:${var.docker_version}"
-      #      cpu       = 128
-      #      memory    = 128
-      essential = true
-
-      environment : [
-        { "name" : "LOGLEVEL", "value" : "DEBUG"},
-        { "name" :"DATA_SERVICE_URL", "value" : "https://pvoutput.org/"},
-        { "name" : "PROVIDER", "value" : "pvoutput.org"}
-      ]
-
-      secrets : [
-        {
-          "name" : "API_KEY",
-          "valueFrom" : "${data.aws_secretsmanager_secret_version.pv-api-version.arn}:api_key::",
-        },
-        {
-          "name" : "SYSTEM_ID",
-          "valueFrom" : "${data.aws_secretsmanager_secret_version.pv-api-version.arn}:system_id::",
-        },
-        {
-          "name" : "DB_URL",
-          "valueFrom" : "${var.database_secret.arn}:url::",
-        },
-        {
-          "name" : "DB_URL_FORECAST",
-          "valueFrom" : "${var.database_secret_forecast.arn}:url::",
-        }
-      ]
-
-      logConfiguration : {
-        "logDriver" : "awslogs",
-        "options" : {
-          "awslogs-group" : var.log-group-name,
-          "awslogs-region" : var.region,
-          "awslogs-stream-prefix" : "streaming"
-        }
-      }
-    },
-    {
       name  = "pv-ss-consumer"
       image = "openclimatefix/pvconsumer:${var.docker_version_ss}"
       #      cpu       = 128
