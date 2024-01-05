a39cdd85cf9cb4b932a12570666b4eaabb5ae67d
devsjc <47188100+devsjc@users.noreply.github.com>
2024-01-05 22:37:36 +0000

    Add API to india development (#380)
    * Add API to india development
    
    * Update incorrect variables in eb_app

---
+++ terraform/modules/services/eb_app/variables.tf
@@ -0,0 +1,78 @@
+// AWS configuration ------------------------------------------------------
+
+variable aws-region {
+  type = string
+  description = "AWS region"
+}
+
+variable aws-environment {
+  type = string
+  description = "Deployment environment"
+}
+
+variable aws-vpc_id {
+  type = string
+  description = "ID of the VPC in which to run the EB app"
+}
+
+variable aws-subnet_id {
+  type = string
+  description = "Subnet ID on which to run the EB app"
+}
+
+// Container configuration ---------------------------------------------------
+
+variable container-registry {
+  type = string
+  description = "Container registry where container resides"
+  default = "ghcr.io"
+}
+
+variable container-name {
+  type = string
+  description = "Container name"
+}
+
+variable container-tag {
+  type = string
+  description = "Container image tag"
+  default = "latest"
+}
+
+variable container-env_vars {
+  type = list(object({
+      name = string
+      value = string
+  }))
+  description = <<EOT
+    container-env_vars = {
+      name : "Name of the environment variable"
+      value : "Value of the environment variable"
+    }
+    container-env_vars : "Environment variables to be set in the container"
+  EOT
+}
+
+variable container-command {
+  type = list(string)
+  description = "Command to run in the container"
+}
+
+// EB configuration --------------------------------------------------------
+
+variable eb-app_name {
+  type = string
+  description = "Name of the EB app"
+}
+
+variable eb-app_type {
+  type = string
+  description = "Type of the EB app"
+  default = "app"
+}
+
+variable eb-instance_type {
+  type = string
+  description = "Instance Type of the EB app"
+  default = "t3.small"
+}
