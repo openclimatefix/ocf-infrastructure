c99a90279e60b09e31dc695414b8489257ba04e3
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2024-02-01 10:32:10 +0000

    Issue/inda data consumer (#413)
    * first try and ruvnl data consumer
    
    * add dag
    
    * fix
    
    * upgrade to 15.5 postgres version
    
    * update india dags
    
    * fix container register

--- terraform/modules/services/airflow/dags/india/forecast-site-dag.py
+++ terraform/modules/services/airflow/dags/india/forecast-site-dag.py
@@ -9,7 +9,7 @@ from airflow.operators.latest_only import LatestOnlyOperator
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.utcnow() - timedelta(hours=25),
+    'start_date': datetime.utcnow() - timedelta(hours=3),
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
     'max_active_runs':10,
@@ -23,7 +23,7 @@ security_group = os.getenv("ECS_SECURITY_GROUP")
 cluster = f"india-ecs-cluster-development"
 
 
-with DAG('site-forecast', schedule_interval="0 * * * *", default_args=default_args, concurrency=10, max_active_tasks=10) as dag:
+with DAG('runvl-forecast', schedule_interval="0 * * * *", default_args=default_args, concurrency=10, max_active_tasks=10) as dag:
     dag.doc_md = "Run the forecast"
 
     latest_only = LatestOnlyOperator(task_id="latest_only")
@@ -44,3 +44,5 @@ with DAG('site-forecast', schedule_interval="0 * * * *", default_args=default_ar
         on_failure_callback=on_failure_callback,
      task_concurrency = 10,
     )
+
+    latest_only >> [forecast]
