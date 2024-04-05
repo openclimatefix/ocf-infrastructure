2a929d31190adaedd7e23de0120953878152fdb2
devsjc <47188100+devsjc@users.noreply.github.com>
2024-04-05 15:22:18 +0100

    feat(india): Add production environment

---
+++ terraform/india/production/backend.tf
@@ -0,0 +1,36 @@
+terraform {
+
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "openclimatefix"
+
+        workspaces {
+            name = "india_production_ap-south-1"
+        }
+    }
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+
+        cloudflare = {
+            source = "cloudflare/cloudflare"
+            version = "~> 4.0"
+        }
+    }
+
+    required_version = ">= 0.14.9"
+
+}
+
+provider "aws" {
+    region = var.region
+    default_tags {
+        tags = {
+            environment = local.environment
+            domain = local.domain
+        }
+    }
+}
---
+++ terraform/india/production/variables.tf
@@ -0,0 +1,41 @@
+variable region {
+    type = string
+    default = "ap-south-1"
+    description = "AWS region"
+}
+
+variable version-nwp {
+    type = string
+    default = "0.5.1"
+    description = "Container image tag of the NWP consumer to use: openclimatefix/nwp-consumer"
+}
+
+variable version-india_api {
+    type = string
+    default = "0.1.0"
+    description = "Container image tag of the India API to use: openclimatefix/india-api"
+}
+
+variable apikey-slack {
+    type = string
+    default = "not-set"
+    description = "Slack API key"
+}
+
+variable version-forecast {
+    type = string
+    default = "0.1.0"
+    description = "Container image tag of the forecast to use: openclimatefix/forecast"
+}
+
+variable version-runvl-consumer {
+    type = string
+    default = "0.0.1"
+    description = "Container image tag of the runvl data consumer to use: openclimatefix/ruvnl_consumer_app"
+}
+
+
+variable "analysis_dashboard_version" {
+    description = "The Analysis Dashboard version"
+    default = "main"
+}
