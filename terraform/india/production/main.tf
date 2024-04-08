1bc9dd80a8a3ea0253fbb8a6f117106e16d6fb01
devsjc <47188100+devsjc@users.noreply.github.com>
2024-04-08 10:33:54 +0100

    feat(india prod): Sync with india dev

--- terraform/india/production/main.tf
+++ terraform/india/production/main.tf
@@ -9,6 +9,9 @@
 # 3.1 - ECS task definition for the NWP consumer
 # 3.2 - ECS task definition for Collection RUVNL data
 # 3.3 - ECS task definition for the Forecast
+# 4.0 - Airflow EB Instance
+# 5.0 - India API EB Instance
+# 5.1 - India Analysis Dashboard
 
 locals {
   environment = "production"
@@ -172,5 +175,67 @@ module "forecast" {
   }
   loglevel      = "INFO"
   ecs-task_execution_role_arn = module.ecs-cluster.ecs_task_execution_role_arn
+}
+
+
+# 4.0
+module "airflow" {
+  source                    = "../../modules/services/airflow"
+  aws-environment           = local.environment
+  aws-region                = local.region
+  aws-domain                = local.domain
+  aws-vpc_id                = module.network.vpc_id
+  aws-subnet_id             = module.network.public_subnet_ids[0]
+  airflow-db-connection-url = "${module.postgres-rds.instance_connection_url}/airflow"
+  docker-compose-version    = "0.0.7"
+  ecs-subnet_id             = module.network.public_subnet_ids[0]
+  ecs-security_group        = module.network.default_security_group_id
+  aws-owner_id              = module.network.owner_id
+  slack_api_conn            = var.apikey-slack
+  dags_folder               = "india"
+}
+
+# 5.0
+module "india-api" {
+  source             = "../../modules/services/eb_app"
+  domain             = local.domain
+  aws-region         = local.region
+  aws-environment    = local.environment
+  aws-subnet_id      = module.network.public_subnet_ids[0]
+  aws-vpc_id         = module.network.vpc_id
+  container-command  = []
+  container-env_vars = [
+    { "name" : "SOURCE", "value" : "indiadb" },
+    { "name" : "PORT", "value" : "80" },
+    { "name" : "DB_URL", "value" : module.postgres-rds.default_db_connection_url},
+  ]
+  container-name = "india-api"
+  container-tag  = var.version-india_api
+  eb-app_name    = "india-api"
+}
+
+# 5.1
+module "analysis_dashboard" {
+  source             = "../../modules/services/eb_app"
+  domain             = local.domain
+  aws-region         = var.region
+  aws-environment    = local.environment
+  aws-subnet_id      = module.network.public_subnet_ids[0]
+  aws-vpc_id         = module.network.vpc_id
+  container-command  = ["streamlit", "run", "main_india.py", "--server.port=8501", "--browser.serverAddress=0.0.0.0", "--server.address=0.0.0.0", "–server.enableCORS False"]
+  container-env_vars = [
+    { "name" : "DB_URL", "value" :  module.postgres-rds.default_db_connection_url},
+    { "name" : "SITES_DB_URL", "value" :  module.postgres-rds.default_db_connection_url},
+    { "name" : "ORIGINS", "value" : "*" },
+  ]
+  container-name = "uk-analysis-dashboard"
+  container-tag  = var.analysis_dashboard_version
+  container-registry = "ghcr.io/openclimatefix"
+  container-port = 8501
+  eb-app_name    = "analysis-dashboard"
+  eb-instance_type = "t3.small"
+  s3_nwp_bucket = {
+    bucket_read_policy_arn = module.s3-nwp-bucket.read_policy_arn
+  }
 }
 
