b77cb6a6931f911cd7eddfb2d5fe163bc8a739e1
peterdudfield <peter.dudfield@hotmail.com>
2024-04-19 16:29:30 +0100

    fix

--- terraform/nowcasting/development/main.tf
+++ terraform/nowcasting/development/main.tf
@@ -372,8 +372,8 @@ module "analysis_dashboard" {
   eb-app_name    = "internal-ui"
   eb-instance_type = "t3.small"
   s3_bucket = [
-    { bucket_read_policy_arn = module.s3.iam-policy-s3-nwp-read.arn }
-    { bucket_read_policy_arn = module.s3.iam-policy-s3-sat-read.arn },
+    { bucket_read_policy_arn = module.s3.iam-policy-s3-nwp-read.arn },
+    { bucket_read_policy_arn = module.s3.iam-policy-s3-sat-read.arn }
   ]
 }
 
