a47fad104bb73e6526767e615afdb799bd7b76ee
peterdudfield <peter.dudfield@hotmail.com>
2024-05-13 17:07:58 +0100

    add comma

--- terraform/india/development/main.tf
+++ terraform/india/development/main.tf
@@ -279,7 +279,7 @@ module "analysis_dashboard" {
   eb-app_name    = "analysis-dashboard"
   eb-instance_type = "t3.small"
   s3_bucket = [
-    { bucket_read_policy_arn = module.s3-nwp-bucket.read_policy_arn }
+    { bucket_read_policy_arn = module.s3-nwp-bucket.read_policy_arn }, 
   ]
 }
 
