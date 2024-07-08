ed97f98e2cec2c64a233558245381d59581691ed
braddf <bradbdf@gmail.com>
2024-07-08 16:36:01 +0100

    add missing "environment" var to sites APIs

--- terraform/nowcasting/development/main.tf
+++ terraform/nowcasting/development/main.tf
@@ -484,6 +484,7 @@ module "pvsite_api" {
     { "name" : "AUTH0_API_AUDIENCE", "value" : var.auth_api_audience },
     { "name" : "AUTH0_DOMAIN", "value" : var.auth_domain },
     { "name" : "AUTH0_ALGORITHM", "value" : "RS256" },
+    { "name" : "ENVIRONMENT", "value" : "development" },
   ]
   container-name = "nowcasting_site_api"
   container-tag  = var.pvsite_api_version
