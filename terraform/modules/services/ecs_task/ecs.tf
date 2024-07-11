20a40b48f9f0d72c7783adbd6a1e22fc8ecbcb08
devsjc <47188100+devsjc@users.noreply.github.com>
2024-07-11 12:34:21 +0100

    fix(ecs-task): Remove comma

--- terraform/modules/services/ecs_task/ecs.tf
+++ terraform/modules/services/ecs_task/ecs.tf
@@ -16,7 +16,7 @@ resource "aws_ecs_task_definition" "task_def" {
   }
 
   volume {
-    name = "${var.ecs-task_name}-temp-data",
+    name = "${var.ecs-task_name}-temp-data"
     docker_volume_configuration {
       scope = "task",
       driver = "local",
