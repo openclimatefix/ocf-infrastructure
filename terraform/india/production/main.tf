5255d9830b7d147c01ca53a230196d99205d36ed
peterdudfield <peter.dudfield@hotmail.com>
2024-08-01 21:13:08 +0100

    upgrade airflow

--- terraform/india/production/main.tf
+++ terraform/india/production/main.tf
@@ -224,7 +224,7 @@ module "forecast" {
 
 # 4.0
 module "airflow" {
-  source                    = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=e4534fb"
+  source                    = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=07ae229"
   aws-environment           = local.environment
   aws-region                = local.region
   aws-domain                = local.domain
