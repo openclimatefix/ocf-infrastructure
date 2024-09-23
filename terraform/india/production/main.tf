4fca299403e4128ad59321e43b3a01ac134da78e
Sukhil Patel <42407101+Sukh-P@users.noreply.github.com>
2024-09-23 17:09:06 +0100

    Update git hash for airflow (#628)

--- terraform/india/production/main.tf
+++ terraform/india/production/main.tf
@@ -358,7 +358,7 @@ module "forecast-ad" {
 
 # 5.0
 module "airflow" {
-  source                    = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=51a7133"
+  source                    = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=f238f2d"
   aws-environment           = local.environment
   aws-region                = local.region
   aws-domain                = local.domain
