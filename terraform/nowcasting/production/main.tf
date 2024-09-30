8c8658bca4c04d4768586d9f7334e21cb80a2458
peterdudfield <peter.dudfield@hotmail.com>
2024-09-30 10:57:06 +0100

    pin uk db version

--- terraform/nowcasting/production/main.tf
+++ terraform/nowcasting/production/main.tf
@@ -121,6 +121,7 @@ module "database" {
   environment          = local.environment
   db_subnet_group_name = module.networking.private_subnet_group_name
   vpc_id               = module.networking.vpc_id
+  engine_version       = "15.7"
 }
 
 # 3.0
