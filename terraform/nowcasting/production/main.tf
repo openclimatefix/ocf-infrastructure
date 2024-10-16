cc4028268f2164576d2d93723d7f46f97ab36df4
peterdudfield <peter.dudfield@hotmail.com>
2024-10-16 16:48:15 +0100

    fix sat secret name

--- terraform/nowcasting/production/main.tf
+++ terraform/nowcasting/production/main.tf
@@ -141,7 +141,7 @@ resource "aws_secretsmanager_secret" "satellite_consumer_secret" {
 
 import {
   to = aws_secretsmanager_secret.satellite_consumer_secret
-  id = "arn:aws:secretsmanager:eu-west-1:752135663966:secret:production/consumer/sat-Yxeyc2"
+  id = "arn:aws:secretsmanager:eu-west-1:752135663966:secret:production/data/sat-Yxeyc2"
 }
 
 # 3.2
