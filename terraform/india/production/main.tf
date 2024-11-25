29a0fe3a24d227c47b81715f9b1617f57f7774b4
Sukhil Patel <42407101+Sukh-P@users.noreply.github.com>
2024-11-25 13:14:20 +0000

    Increase memory for ru forecaster prod (#695)
    Update main.tf

--- terraform/india/production/main.tf
+++ terraform/india/production/main.tf
@@ -330,7 +330,7 @@ module "forecast" {
   ecs_config  = {
     docker_image   = "openclimatefix/india_forecast_app"
     docker_version = var.version-forecast
-    memory_mb      = 2048
+    memory_mb      = 4096
     cpu            = 1024
   }
   rds_config = {
