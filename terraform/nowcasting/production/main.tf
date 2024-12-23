69dc214aaab312ff289e212f388996ac32e103fc
peterdudfield <peter.dudfield@hotmail.com>
2024-12-23 08:53:27 +0000

    role bk uk, upgrade india

--- terraform/nowcasting/production/main.tf
+++ terraform/nowcasting/production/main.tf
@@ -681,7 +681,7 @@ module "forecast_blend" {
 
 # 5.2
 module "airflow" {
-  source = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=7e27e14"
+  source = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=2c9c35f"
 
   aws-environment   = local.environment
   aws-domain        = local.domain
