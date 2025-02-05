c7edd6cdc35aa8d062539cdeb8baae325f1b9c6c
peterdudfield <peter.dudfield@hotmail.com>
2025-02-05 21:19:46 +0000

    upgrade airflow to 631a791

--- terraform/nowcasting/production/main.tf
+++ terraform/nowcasting/production/main.tf
@@ -705,7 +705,7 @@ module "forecast_blend" {
 
 # 5.2
 module "airflow" {
-  source = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=2b68542"
+  source = "github.com/openclimatefix/ocf-infrastructure//terraform/modules/services/airflow?ref=631a791"
 
   aws-environment   = local.environment
   aws-domain        = local.domain
