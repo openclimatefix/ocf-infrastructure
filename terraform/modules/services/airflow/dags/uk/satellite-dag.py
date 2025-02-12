8e5bcf381f2136bac7826ea53b26a3c29fccadca
Aryan Bhosale <36108149+aryanbhosale@users.noreply.github.com>
2025-02-12 17:47:42 +0530

    15 min filecheck added (#783)
    * 15 min filecheck added
    
    * no err for 5min fails,
    
    * emoji
    
    * rm warning
    
    * rm on failure callbck
    
    * message update
    
    * Update satellite-dag.py

--- terraform/modules/services/airflow/dags/uk/satellite-dag.py
+++ terraform/modules/services/airflow/dags/uk/satellite-dag.py
@@ -26,21 +26,24 @@ cluster = f"Nowcasting-{env}"
 
 satellite_error_message = (
     "⚠️ The task {{ ti.task_id }} failed. "
-    "But its ok, the forecast will automatically move over to a PVNET-ECMWF, "
-    "which doesnt need satellite data. "
+    "But it's OK, the forecast will automatically move over to PVNET-ECMWF, "
+    "which doesn't need satellite data. "
     "EUMETSAT status links are <https://uns.eumetsat.int/uns/|here> "
     "and <https://masif.eumetsat.int/ossi/webpages/level3.html?ossi_level3_filename=seviri_rss_hr.html&ossi_level2_filename=seviri_rss.html|here>. "
-    "No out of office hours support is required, but please log in an incident log."
+    "No out-of-hours support is required, but please log in an incident log."
+)
+
+satellite_both_files_missing_error_message = (
+    "⚠️ Tried to update the database to show when the latest satellite data was collected, " 
+    "but could not find the 5-min or the 15-min satellite files."
 )
 
 satellite_clean_up_error_message = (
-    "⚠️ The task {{ ti.task_id }} failed. " 
-    "But its ok, this is only used for cleaning up the EUMETSAT customisation, "
-    "but the satellite consumer should also do this. "
-    "No out of office hours support is required."
+    "⚠️ The task {{ ti.task_id }} failed. "
+    "But it's OK, this is only used for cleaning up the EUMETSAT customisation, "
+    "and the satellite consumer should also do this. "
+    "No out-of-hours support is required."
 )
-
-# Tasks can still be defined in terraform, or defined here
 
 region = "uk"
 
@@ -56,6 +59,7 @@ with DAG(
     concurrency=10,
     max_active_tasks=10,
     start_date=datetime.now(tz=timezone.utc) - timedelta(hours=0.5),
+    catchup=False,
 ) as dag:
     dag.doc_md = "Get Satellite data"
 
@@ -81,14 +85,31 @@ with DAG(
         awslogs_region="eu-west-1",
     )
 
-    file = f"s3://nowcasting-sat-{env}/data/latest/latest.zarr.zip"
-    command = f'curl -X GET "{url}/v0/solar/GB/update_last_data?component=satellite&file={file}"'
-    satellite_update = BashOperator(
-        task_id=f"{region}-satellite-update",
-        bash_command=command,
+    file_5min = f"s3://nowcasting-sat-{env}/data/latest/latest.zarr.zip"
+    command_5min = (
+        f'curl -X GET '
+        f'"{url}/v0/solar/GB/update_last_data?component=satellite&file={file_5min}"'
     )
 
-    latest_only >> sat_consumer >> satellite_update
+    satellite_update_5min = BashOperator(
+        task_id=f"{region}-satellite-update-5min",
+        bash_command=command_5min,
+    )
+
+    file_15min = f"s3://nowcasting-sat-{env}/data/latest/latest_15.zarr.zip"
+    command_15min = (
+        f'curl -X GET '
+        f'"{url}/v0/solar/GB/update_last_data?component=satellite&file={file_15min}"'
+    )
+
+    satellite_update_15min = BashOperator(
+        task_id=f"{region}-satellite-update-15min",
+        bash_command=command_15min,
+        trigger_rule="all_failed",
+        on_failure_callback=slack_message_callback(satellite_both_files_missing_error_message),
+    )
+
+    latest_only >> sat_consumer >> satellite_update_5min >> satellite_update_15min
 
 with DAG(
     f"{region}-national-satellite-cleanup",
@@ -97,12 +118,13 @@ with DAG(
     concurrency=10,
     max_active_tasks=10,
     start_date=datetime.now(tz=timezone.utc) - timedelta(hours=7),
-) as dag:
-    dag.doc_md = "Satellite data clean up"
+    catchup=False,
+) as dag_cleanup:
+    dag_cleanup.doc_md = "Satellite data clean up"
 
-    latest_only = LatestOnlyOperator(task_id="latest_only")
+    latest_only_cleanup = LatestOnlyOperator(task_id="latest_only")
 
-    sat_consumer = EcsRunTaskOperator(
+    sat_consumer_cleanup = EcsRunTaskOperator(
         task_id=f"{region}-national-satellite-cleanup",
         task_definition="sat-clean-up",
         cluster=cluster,
@@ -122,7 +144,6 @@ with DAG(
         awslogs_region="eu-west-1",
     )
 
-    latest_only >> sat_consumer
-
+    latest_only_cleanup >> sat_consumer_cleanup
 
 
