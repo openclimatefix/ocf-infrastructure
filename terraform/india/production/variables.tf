8cd4f753feb2f24e883d32346a55868859aa4b3b
devsjc <47188100+devsjc@users.noreply.github.com>
2025-03-31 12:34:43 +0100

    refactor(main): Remove unused variables

--- terraform/india/production/variables.tf
+++ terraform/india/production/variables.tf
@@ -1,45 +1,20 @@
-variable region {
+variable "region" {
     type = string
     default = "ap-south-1"
     description = "AWS region"
 }
 
-variable version-nwp {
-    type = string
-    default = "0.5.1"
-    description = "Container image tag of the NWP consumer to use: openclimatefix/nwp-consumer"
-}
-
-variable version-india_api {
+variable "version-india_api" {
     type = string
     default = "0.1.0"
     description = "Container image tag of the India API to use: openclimatefix/india-api"
 }
 
-variable apikey-slack {
+variable "apikey-slack" {
     type = string
     default = "not-set"
     description = "Slack API key"
 }
-
-variable version-forecast {
-    type = string
-    default = "0.1.0"
-    description = "Container image tag of the forecast to use: openclimatefix/forecast"
-}
-
-variable version-forecast-ad {
-  type = string
-  default = "0.1.0"
-  description = "Container image tag of the forecast ad to use: openclimatefix/forecast"
-}
-
-variable version-runvl-consumer {
-    type = string
-    default = "0.0.1"
-    description = "Container image tag of the runvl data consumer to use: openclimatefix/ruvnl_consumer_app"
-}
-
 
 variable "analysis_dashboard_version" {
     description = "The Analysis Dashboard version"
@@ -68,13 +43,8 @@ variable "auth_api_audience" {
   default = "not-set"
 }
 
-variable satellite-consumer {
-  type = string
-  default = "0.0.1"
-  description = "Container image tag of the satellite data consumer to use: openclimatefix/satip"
-}
-
 variable "auth_dashboard_client_id" {
   description = "The Auth client id for the dashboard that should be used"
   default     = "not-set"
 }
+
