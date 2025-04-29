e6e4b6c0e7b29381d269ed93ba25c21097f4607c
peterdudfield <peter.dudfield@hotmail.com>
2025-04-29 09:56:28 +0100

    rolling back 17 to 15 for db

--- terraform/nowcasting/production/main.tf
+++ terraform/nowcasting/production/main.tf
@@ -72,7 +72,7 @@ module "database" {
   environment          = local.environment
   db_subnet_group_name = module.networking.private_subnet_group_name
   vpc_id               = module.networking.vpc_id
-  engine_version       = "17.2"
+  engine_version       = "15.8"
 }
 
 # 1.1
@@ -177,7 +177,7 @@ module "pvsite_database" {
   db_name                     = "pvsite"
   rds_instance_class          = "db.t3.small"
   allow_major_version_upgrade = true
-  engine_version = "17.2"
+  engine_version = "15.8"
 }
 
 # 5.2
