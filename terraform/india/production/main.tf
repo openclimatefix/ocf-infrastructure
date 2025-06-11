766ecd981808481c185452b394b16a677917c9c5
peterdudfield <peter.dudfield@hotmail.com>
2025-06-11 10:30:39 +0100

    rds prod india 17.4

--- terraform/india/production/main.tf
+++ terraform/india/production/main.tf
@@ -40,7 +40,7 @@ module "postgres-rds" {
   db_name              = "indiadb"
   rds_instance_class   = "db.t3.small"
   allow_major_version_upgrade  = true
-  engine_version = "17.2"
+  engine_version = "17.4"
 }
 
 # 1.2
