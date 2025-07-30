0313517b6091dc8ec904bde042ecfcde12b52760
peterdudfield <peter.dudfield@hotmail.com>
2025-07-30 16:07:08 +0100

    add route volume of gp3

--- terraform/modules/services/eb_app/eb.tf
+++ terraform/modules/services/eb_app/eb.tf
@@ -28,6 +28,20 @@ resource "aws_elastic_beanstalk_environment" "eb-environment" {
     resource  = ""
   }
 
+  setting {
+    namespace   = "aws:autoscaling:launchconfiguration"
+    name        = "RootVolumeType"
+    value       = "gp3"
+    resource    = ""
+  }
+
+  setting {
+    namespace   = "aws:autoscaling:launchconfiguration"
+    name        = "RootVolumeSize"
+    value       = var.ec2-storage-gb
+    resource    = ""
+  }
+
   # the next line IS NOT RANDOM,
   # see https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts.platforms.html
   solution_stack_name = "64bit Amazon Linux 2 v3.8.0 running Docker"
@@ -46,13 +60,6 @@ resource "aws_elastic_beanstalk_environment" "eb-environment" {
             resource  = ""
         }
     }
-
-  setting {
-    namespace   = "aws:autoscaling:launchconfiguration"
-    name        = "RootVolumeSize"
-    value       = var.ec2-storage-gb
-    resource    = ""
-  }
 
   setting {
     namespace = "aws:ec2:vpc"
