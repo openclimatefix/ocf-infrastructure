462a3b438b67961a7cef4dc7efe31f8c0173120a
Peter Dudfield <peter.dudfield@hotmail.com>
2025-08-29 11:55:40 +0100

    fix typo

--- terraform/modules/services/lambda_api/iam.tf
+++ terraform/modules/services/lambda_api/iam.tf
@@ -14,4 +14,4 @@ resource "aws_iam_role" "lambda" {
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}s=
+}
