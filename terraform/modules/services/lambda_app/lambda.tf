03f4d3495034deae9e346d1ae6145673e680731a
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-08-29 15:13:52 +0100

    api open quartz solar on lambda (#897)
    * first try at api on lambda
    
    * fix typo
    
    * tidy up
    
    * remove a dependency
    
    * add concurrently limit and tags
    
    * fix
    
    * use version

---
+++ terraform/modules/services/lambda_app/lambda.tf
@@ -0,0 +1,55 @@
+# --- Lambda ---
+
+resource "aws_cloudwatch_log_group" "api" {
+  name              = "/aws/lambda/${var.app_name}-api"
+  retention_in_days = 14
+}
+
+resource "aws_lambda_function" "api" {
+  function_name    = "${var.app_name}-api"
+  role             = aws_iam_role.lambda.arn
+  image_uri        = "${var.container-registry}/${var.container-name}:${var.container-tag}"
+  package_type     = "Image"
+  timeout          = 10
+
+  environment {
+    variables = {}
+  }
+
+  tags = {
+    name = "${var.app_name}"
+    type = "lambda"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.api,
+  ]
+}
+
+resource "aws_lambda_provisioned_concurrency_config" "lambda_concurrentl_limit" {
+  function_name                     = aws_lambda_function.api.function_name
+  provisioned_concurrent_executions = 1
+  qualifier                         = aws_lambda_function.api.version
+}
+
+
+# --- Lambda Endpoint ---
+
+resource "aws_lambda_function_url" "api" {
+  function_name      = aws_lambda_function.api.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
+
+output "api_url" {
+  value = aws_lambda_function_url.api.function_url
+}
---
+++ terraform/modules/services/lambda_app/variables.tf
@@ -0,0 +1,31 @@
+variable aws-region {
+  type = string
+  description = "AWS region"
+}
+
+variable aws-environment {
+  type = string
+  description = "Deployment environment"
+}
+
+variable app_name {
+    type = string
+    description = "The application name"
+}
+
+variable container-registry {
+  type = string
+  description = "Container registry where container resides"
+  default = "ghcr.io"
+}
+
+variable container-name {
+  type = string
+  description = "Container name"
+}
+
+variable container-tag {
+  type = string
+  description = "Container image tag"
+  default = "latest"
+}
