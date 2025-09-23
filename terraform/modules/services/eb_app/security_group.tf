7d56534269310224c660216bf28350172ed296c8
Peter Dudfield <peter.dudfield@hotmail.com>
2025-09-23 11:12:26 +0100

    refactor

--- terraform/modules/services/eb_app/security_group.tf
+++ terraform/modules/services/eb_app/security_group.tf
@@ -5,30 +5,34 @@ resource "aws_security_group" "sg" {
   description = "API security group to allow inbound/outbound traffic"
   vpc_id      = var.aws-vpc_id
 
-  ingress {
-    from_port = "80"
-    to_port   = "80"
-    protocol  = "tcp"
-    self      = true
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     Environment = var.aws-environment
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "basic" {
+
+  security_group_id = aws_security_group.sg.id
+  from_port = "80"
+  to_port   = "80"
+  protocol  = "tcp"
+  self      = true
+}
+
+resource "aws_vpc_security_group_egress_rule" "basic" {
+
+  security_group_id = aws_security_group.sg.id
+  from_port        = 0
+  to_port          = 0
+  protocol         = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_traffic_ipv4" {
   count = var.ingress_extra_port != -1 ? 1 : 0
 
-  security_group_id = var.ingress_extra_port.sg.id
+  security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = var.ingress_extra_port
   ip_protocol       = "tcp"
