dfada7424e588981387b9856b8825d581a9c1c5e
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-09-23 15:56:52 +0100

    Ingress dp (#918)
    * hard code ingress
    
    * fix
    
    * tcp

--- terraform/modules/services/eb_app/security_group.tf
+++ terraform/modules/services/eb_app/security_group.tf
@@ -17,9 +17,9 @@ resource "aws_security_group" "sg" {
   ingress {
     from_port        = "50051"
     to_port          = "50051"
-    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+    protocol         = "tcp"
   }
 
   egress {
